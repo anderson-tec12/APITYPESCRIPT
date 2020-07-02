100bcad2b021102a16f67ac620a58d7712c480ba
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-01 22:54:00 -0300

    Criando o metodo Get

--- controller/newController.ts
+++ controller/newController.ts
@@ -1,7 +1,18 @@
 import newService from "../services/newService";
+import * as http from "http-status";
 
 class NewController {
-  get(req, res) {}
+  sendResponse = function (res, statusCode, data) {
+    res.status(statusCode).json({ result: data });
+  };
+
+  get(req, res) {
+    newService
+      .get()
+      .then((news) => this.sendResponse(res, http.OK, news))
+      .catch((error) => console.error.bind(console, `Error ${error}`));
+  }
+
   getById(req, res) {}
   create(req, res) {}
   update(req, res) {}
