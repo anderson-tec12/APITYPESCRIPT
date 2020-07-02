ca4f8da0fe50b620b267273201a8c0cdb47143d6
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-02 07:59:47 -0300

    implemetando os metodos da class com as rotas

--- startUp.ts
+++ startUp.ts
@@ -2,7 +2,9 @@
 
 import * as express from "express";
 import * as bodyParser from "body-parser";
+
 import db from "./infra/db";
+import newController from "./controller/newController";
 
 class StartUp {
   public app: express.Application;
@@ -26,6 +28,13 @@ class StartUp {
     this.app.route("/").get((req, res) => {
       res.send({ versao: "0.0.1" });
     });
+
+    //rotas
+    this.app.route("/api/v1/news").get(newController.get);
+    this.app.route("/api/v1/news/:id").get(newController.getById);
+    this.app.route("/api/v1/news").post(newController.create);
+    this.app.route("/api/v1/news/:id").put(newController.update);
+    this.app.route("/api/v1/news/:id").delete(newController.delete);
   }
 }
 
