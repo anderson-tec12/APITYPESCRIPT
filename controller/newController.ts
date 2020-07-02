9a88d16309b4c05540ac3057417cc6d33bc1d666
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-01 22:45:33 -0300

    Criando a controller

---
+++ controller/newController.ts
@@ -0,0 +1,11 @@
+import newService from "../services/newService";
+
+class NewController {
+  get(req, res) {}
+  getById(req, res) {}
+  create(req, res) {}
+  update(req, res) {}
+  delete(req, res) {}
+}
+
+export default new NewController();
