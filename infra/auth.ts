16d6f4e21ec7252627fa0990737138e23c9c5a56
Andersonbarros.silva <anderson.tec12@gmail.com>
2020-07-03 23:16:50 -0300

    configurando e importando JWT

---
+++ infra/auth.ts
@@ -0,0 +1,26 @@
+import * as jwt from "jsonwebtoken";
+import configs from "./configs";
+
+class auth {
+  validate(req, res, next) {
+    let token = req.headers["x-access-token"];
+
+    if (token) {
+      jwt.verify(token, configs.secret, function (err, decoded) {
+        if (err) {
+          return res.status(403).send({
+            success: false,
+            message: "Token invalido",
+          });
+        } else {
+          next();
+        }
+      });
+    } else {
+      req.status(401).send({
+        success: false,
+        message: "401 sem autorização",
+      });
+    }
+  }
+}
